1ac27075be8b780439aaab42dc9626ff2d3d3dcb
Sean6bucks <sean6bucks@gmail.com>
2015-10-18 15:10:46 +0800

    Change s to author pages
    Changes to author page never committed, CSS updates for better post
    display

--- wordpress/wp-content/themes/scalawag_theme/author.php
+++ wordpress/wp-content/themes/scalawag_theme/author.php
@@ -1,21 +1,9 @@
 <?php get_header(); ?>
 <?php get_template_part( 'minheader' ); ?>
-
-<?php if (have_posts()): the_post(); ?>
 	
-	<div class="articles-headline">
-	    <h3><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h3>
-	</div>
-
-<?php if ( get_the_author_meta('description')) : ?>
-
-<?php echo get_avatar(get_the_author_meta('user_email')); ?>
-
-	<h2><?php _e( 'About ', 'html5blank' ); echo get_the_author() ; ?></h2>
-
-	<?php echo wpautop( get_the_author_meta('description') ); ?>
-
-<?php endif; ?>
+<div class="articles-headline">
+    <h3><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h3>
+</div>
 
 <div class="posts-wrapper">
 
@@ -24,20 +12,5 @@
     <?php get_template_part('pagination'); ?>
 
 </div>
-	<!-- /article -->
-
-
-<?php else: ?>
-
-	<!-- article -->
-	<article>
-
-		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-	</article>
-	<!-- /article -->
-
-<?php endif; ?>
-
 
 <?php get_footer(); ?>
